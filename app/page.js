6920f7336de6f27408f2779358fd13722e2350b9
mo-dhia <med.d.bouthuri@gmail.com>
2023-09-25 13:12:53 +0100

    arrivas drag & drop fully functional with auto set bonus

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 "use client"
 import { useEffect, useRef, useState } from "react"
-import { resizeEvent, scrollTrigger, scrollEvent } from '@/functions/vawzen/functions'
+import { resizeEvent, scrollTrigger, scrollEvent } from '@/functions/vawzen/scroll-interaction'
 
 import About from "./landing_page/2_about/about"
 import Collection from "./landing_page/3_collection/collection"
 import collection_data from "@/data/landing/3_collection.json"
-
+import Arrivals from './landing_page/4_arrivals/arrivals'
 import { animations } from "@/functions/landing_functions/landing_functions"
 export default function Home() {
   const [scrollY, setScrollY] = useState(0);
@@ -29,6 +29,7 @@ export default function Home() {
           <div style={{ height: '100vw' }} />
           <About _ref={about} />
           <Collection _ref={collection} />
+          <Arrivals />
           <div style={{ height: '100vw' }} />
         </div>
       </main>
